411e1448bc0e0d3ee6e1c945a8ec79db60a4b67b
voda <datalyticsgis@gmail.com>
2022-01-29 19:50:36 +0200

    update secrets

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -3,7 +3,7 @@ dotenv.config({ path: ".env" });
 
 export const ENVIRONMENT = process.env.NODE_ENV || "development";
 export const PORT = process.env.PORT;
-export const JWT_SECRET = process.env.JWT_SECRET_KEY;
+export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRY = process.env.JWT_EXPIRY;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_USER = process.env.DB_USER;
