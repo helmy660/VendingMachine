b59b90fee4adf97cb995e726e696e5e1a887a45c
voda <datalyticsgis@gmail.com>
2022-01-30 23:05:02 +0200

    Add new custom errors

--- src/services/customError.ts
+++ src/services/customError.ts
@@ -18,52 +18,60 @@ export class CustomError extends Error {
 }
 
 export const ErrorTypes = {
-  INVALID_REQUEST_BODY: {
-    code: 4001,
-    message: "Error: Invalid data in request body",
-  },
   INVALID_CREDENTIALS: {
-    code: 4002,
+    code: 4001,
     message: "Error: Invalid username or password",
   },
-  INVALID_USERNAME: {
-    code: 4003,
-    message: "Error: Invalid username",
-  },
   INVALID_USER_ID: {
-    code: 4004,
+    code: 4002,
     message: "Error: Invalid user id",
   },
-  USER_NOT_AUTHORIZED: {
-    code: 4005,
-    message: "Error: This User is not authorized to perform this action",
-  },
   INVALID_ACTION: {
-    code: 4006,
+    code: 4003,
     message: "Error: Failed to perform this action",
   },
-  INVALID_QUANTITY: {
-    code: 4007,
-    message: "Error: Invalid quantity. Only accepts 5,10,20,50,100 cent coins",
-  },
   INVALID_PRODUCT: {
-    code: 4008,
+    code: 4004,
     message: "Error: Invalid product",
   },
-  Empty_DEPOSIT: {
-    code: 4009,
-    message: "Error: You don not have enough deposit. Please recharge",
-  },
   INVALID_SELLER: {
-    code: 4010,
+    code: 4005,
     message: "Error: Invalid seller",
   },
-  OUT_OF_STOCK: {
+  USER_NOT_AUTHORIZED: {
+    code: 4006,
+    message: "Error: This User is not authorized to perform this action",
+  },
+  USER_EMPTY_DEPOSIT: {
+    code: 4007,
+    message: "Error: You don not have enough deposit. Please recharge",
+  },
+  PRODUCT_ADDING: {
+    code: 4008,
+    message: "Error: Failed to add this product",
+  },
+  PRODUCT_COST_UPDATING: {
+    code: 4009,
+    message: "Error: Failed to update the cost of this product",
+  },
+  PRODUCT_AMOUNT_UPDATING: {
+    code: 4010,
+    message: "Error: Failed to update the amount of this product",
+  },
+  PRODUCT_EXISTING: {
     code: 4011,
-    message: "Error: Those items are out of stock.",
+    message: "Error: This product is already existing, please select another name",
+  },
+  PRODUCT_OUT_OF_STOCK: {
+    code: 4012,
+    message: "Error: This item is out of stock.",
+  },
+  PRODUCT_AVAILABLE_AMOUNT: {
+    code: 4013,
+    message: "Error: No enough amount for this prodcut to perform this action",
   },
   VALIDATION_ERROR: {
-    code: 4012,
+    code: 4014,
     message: "Validation Error",
   },
 };
